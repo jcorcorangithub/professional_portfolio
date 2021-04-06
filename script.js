9cc5e1e40d7a6b49e403ba76baa243661b3419e3
James Corcoran <jpcsoccer1234@gmail.com>
2021-04-06 17:01:11 -0400

    got rid of js function for sticky navbar, did it all on css, added padding-top to h1 so it did not overlap with navbar

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-// Get the navbar
-// var navbar = document.querySelector("nav");
-// var navbar = $("nav");
+// // Get the navbar
+// // var navbar = document.querySelector("nav");
+// // var navbar = $("nav");
 
-// Get the offset position of the navbar
-var sticky = document.querySelector("nav").offsetTop;
+// // Get the offset position of the navbar
+// var sticky = document.querySelector("nav").offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    //navbar.classList.add("sticky")
-    $("nav").addClass("sticky");
-  } else {
-    //navbar.classList.remove("sticky");
-    $("nav").removeClass("sticky");
-  }
-}
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     //navbar.classList.add("sticky")
+//     $("nav").addClass("sticky");
+//   } else {
+//     //navbar.classList.remove("sticky");
+//     $("nav").removeClass("sticky");
+//   }
+// }
